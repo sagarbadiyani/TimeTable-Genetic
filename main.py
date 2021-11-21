97ad9be781ffc8701036e3cde278217a60fb5076
Sagar Badiyani <sagar.a.badiyani@gmail.com>
2021-11-21 19:50:03 +0530

    Add preprocess file

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-import cfg
-
-from file_handling.section_capacity_parser import get_course_section_capacity_df
-from file_handling.tt_data_parser import get_parsed_tt_df
-
-
 def main():
-    cfg.course_section_capacity_df = get_course_section_capacity_df()
-    cfg.tt_parsed_df = get_parsed_tt_df()
-
+    pass
 
 if __name__ == "__main__":
     main()
