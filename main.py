3427c031681eb9a6a5ff6f1defd37017aebf0885
Sagar Badiyani <sagar.a.badiyani@gmail.com>
2021-11-21 19:10:14 +0530

    Fix file path

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from file_handling.section_capacity_input import get_course_section_capacity_df
 
 
 def main():
-    cfg.get_course_section_capacity_df = get_course_section_capacity_df()
+    cfg.course_section_capacity_df = get_course_section_capacity_df()
 
 
 if __name__ == "__main__":
