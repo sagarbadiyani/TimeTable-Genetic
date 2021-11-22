69913d4503f8d1212be28573036202f14e605dd4
Sagar Badiyani <sagar.a.badiyani@gmail.com>
2021-11-22 14:45:05 +0530

    Add fitness template

---
+++ fitness/fitness.py
@@ -0,0 +1,2 @@
+def get_fitness(updated_fields):
+    pass
--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from preprocess import preprocess
 from field_operation.field_update import get_updated_field
+from fitness.fitness import get_fitness
 
 
 def main():
     preprocess()
     arr = [15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
     updated_fields = get_updated_field(arr)
+    fitness = get_fitness(updated_fields)
 
 
 if __name__ == "__main__":
