fa5a1f11c35858b2b67059e3d735737a28fce2ee
Sagar Badiyani <sagar.a.badiyani@gmail.com>
2021-11-22 14:01:39 +0530

    Add Field updatation

---
+++ field_operation/field_update.py
@@ -0,0 +1,42 @@
+import copy
+import cfg
+
+from cfg import course_list_all, course_list_pure_eg, course_list_pure_ws, course_list_eg, course_list_ws
+
+
+def get_updated_field(arr):
+    updated_eg_course_list = []
+    updated_eg_course_indices_list = []
+    updated_ws_course_list = []
+    updated_ws_course_indices_list = []
+
+    for i in arr:
+        course = course_list_all[i]
+        if course not in course_list_pure_ws:
+            updated_eg_course_list.append(course)
+            updated_eg_course_indices_list.append(i)
+        if course not in course_list_pure_eg:
+            updated_ws_course_list.append(course)
+            updated_ws_course_indices_list.append(i)
+
+    copied_field_eg = copy.deepcopy(cfg.field_eg)
+    copied_field_ws = copy.deepcopy(cfg.field_ws)
+    updated_field_eg = copy.deepcopy(cfg.field_eg)
+    updated_field_ws = copy.deepcopy(cfg.field_ws)
+
+    for idx, course in enumerate(updated_eg_course_list):
+        index = course_list_eg.index(course)
+        blob = copied_field_eg[index]
+        updated_field_eg[idx] = blob
+
+    for idx, course in enumerate(updated_ws_course_list):
+        index = course_list_ws.index(course)
+        blob = copied_field_ws[index]
+        updated_field_ws[idx] = blob
+
+    return {
+        "course_list_eg": updated_eg_course_list,
+        "course_list_ws": updated_ws_course_list,
+        "field_eg": updated_field_eg,
+        "field_ws": updated_field_ws
+    }
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from preprocess import preprocess
+from field_operation.field_update import get_updated_field
 
 
 def main():
     preprocess()
+    arr = [15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+    updated_fields = get_updated_field(arr)
 
 
 if __name__ == "__main__":
