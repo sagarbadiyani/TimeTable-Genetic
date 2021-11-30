1f9c7fd8b8fa208985e29ea008a5452f9e70cbbc
Sagar Badiyani <sagar.a.badiyani@gmail.com>
2021-11-30 21:56:57 +0530

    Add time diff print statement

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
+import time
+
 from file_handling import printer
 from genetic import genetic_algorithm
 from preprocessing.preprocess import preprocess
 
 
 def main():
+    start_time = time.time()
     preprocess()
     generations, path = genetic_algorithm.start(epochs=500)
     printer.print_results(generations, path)
+    end_time = time.time()
+    print(f"Total time taken: {end_time - start_time} seconds")
 
 
 if __name__ == "__main__":
